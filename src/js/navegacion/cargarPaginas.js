a7cde6ed00922ab74a89558af174446e6b91d1cd
XaniaMendoza <xania.mendoza@ideas4learning.com>
2022-04-12 14:30:33 -0500

    pag07

--- src/js/navegacion/cargarPaginas.js
+++ src/js/navegacion/cargarPaginas.js
@@ -89,8 +89,9 @@ function visualizarPagina(){
           
             // Scrip página06
             visualizarCuadro1();
-            // visualizarCuadro2();
-            // visualizarCuadro3();
+
+            // Scrip página07
+            visualizarPag07()
         });
     }
 }
