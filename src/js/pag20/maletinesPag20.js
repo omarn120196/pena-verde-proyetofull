9062907ac84222e361e75ae36812e987082ac164
Dyjdfg36 <83386679+Dyjdfg36@users.noreply.github.com>
2022-04-20 00:22:52 -0500

    actulizacionde paginas 21-21

---
+++ src/js/pag20/maletinesPag20.js
@@ -0,0 +1,42 @@
+
+
+function mostrarAmarillo(){
+    var elemento = document.getElementById("mAmarillo");
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+        } 
+    else{
+        elemento.style.display = "none"
+    };
+}
+
+function mostrarVerde(){
+    var elemento = document.getElementById("mVerde");
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+    } 
+    else{
+        elemento.style.display = "none"
+    };
+}
+
+function mostrarAzul(){
+    var elemento = document.getElementById("mAzul");
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+    } 
+    else{
+        elemento.style.display = "none"
+    };
+}
+
+function mostrarRojo(){
+    var elemento = document.getElementById("mRojo");
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+    } 
+    else{
+        elemento.style.display = "none"
+    };
+}
+
