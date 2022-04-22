b74a4d50c896e4feadb6659a19f4dcdeb5f5f143
Dyjdfg36 <83386679+Dyjdfg36@users.noreply.github.com>
2022-04-21 19:08:52 -0500

    ultimos cambios5

--- src/js/pag22/mostrarTjPag22.js
+++ src/js/pag22/mostrarTjPag22.js
@@ -1,6 +1,6 @@
 function mostrarEtapa1Pag22(){
-    var elemento1 = document.getElementById("infoAmarilloPag22");
-    if (elemento1.style.display == "none") {
+    var elemento1 = document.getElementById("info1EtapaPag22");
+    if (elemento1.style.display == "") {
         elemento1.style.display = "block"
     }
     else{
@@ -9,7 +9,7 @@ function mostrarEtapa1Pag22(){
 }
 
 function mostrarEtapa2Pag22(){
-    var elemento2 = document.getElementById("infoVerdePag22");
+    var elemento2 = document.getElementById("info2EtapaPag22");
     if (elemento2.style.display == "none") {
         elemento2.style.display = "block"
     }
@@ -19,7 +19,7 @@ function mostrarEtapa2Pag22(){
 }
 
 function mostrarEtapa3Pag22(){
-    var elemento3 = document.getElementById("infoAzulPag22");
+    var elemento3 = document.getElementById("info3EtapaPag22");
     if (elemento3.style.display == "none") {
         elemento3.style.display = "block"
     }
